d568d806f9cb995adb3d59e04fc50ef637972aea
fermeise <ocker.christian@gmail.com>
2014-11-05 15:30:10 +0100

    Benchmark uses log n * log log n sampling factor.

--- benchmark/cst_fully/src/fcst_time_and_space.cpp
+++ benchmark/cst_fully/src/fcst_time_and_space.cpp
@@ -7,6 +7,17 @@ using timer = std::chrono::high_resolution_clock;
 
 typedef cst_fully<cst_sada<csa_wt<wt_huff<rrr_vector<63> >, 32, 32, text_order_sa_sampling<>, text_order_isa_sampling_support<>>, lcp_wt<> > > cst_type;
 
+// Returns ceil(log(n))
+size_t log(size_t n) {
+    size_t m = 1;
+    size_t res = 0;
+    while(n > m) {
+        m *= 2;
+        res++;
+    }
+    return res;
+}
+
 int main(int argc, char** argv) {
     if(argc != 2) {
         std::cout << "Syntax: " << argv[0] << " [input file]" << std::endl;
@@ -17,7 +28,10 @@ int main(int argc, char** argv) {
 
     cst_type::construction_cst_type cst;
     construct(cst, input_file, 1);
-    cst_type fcst(cst, 100, false);
+
+    const size_t n = cst.size();
+    const size_t delta = log(n) * log(log(n));
+    cst_type fcst(cst, delta, false);
 
     std::cout << "# ALPHABET_SIZE = " << fcst.csa.sigma << std::endl;
     std::cout << "# DATA_SIZE = " << fcst.csa.text.size() << std::endl;
