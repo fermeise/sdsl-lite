16e4e673b36821dec945605e6ea875acd7f3c832
fermeise <ocker.christian@gmail.com>
2015-04-10 21:32:39 +0200

    R script generates graph data files.

--- benchmark/cst_fully/visualize/graph_data.R
+++ benchmark/cst_fully/visualize/graph_data.R
@@ -7,6 +7,13 @@ operations = data.frame(id=c("LCA", "LETTER", "SLINK", "CHILD", "DEPTH", "PARENT
 rownames(operations)<-operations[["OP_ID"]]
 colnames(operations)<-c("OP_ID", "LATEX-NAME")
 
+idx_types = data.frame(id=c("FULLY", "FULLY_SDS", "FULLY_BLIND"),
+					   caption=c("cst\\_fully", "cst\\_fully\\_sds", "cst\\_fully\\_blind"))
+rownames(idx_types)<-idx_types[["IDX_ID"]]
+colnames(idx_types)<-c("IDX_ID", "LATEX-NAME")
+
+sampling_rates = c(4, 8, 16, 32, 64, 128)
+
 raw <- data_frame_from_key_value_pairs("../results/all.txt")
 
 for(op_idx in 1:nrow(operations)) {
@@ -14,14 +21,15 @@ for(op_idx in 1:nrow(operations)) {
 	
 	sink(paste("output/", op_name, ".txt", sep="", collapse=""))
 	
-	for(i in 1:nrow(raw)) {
-		data<-raw[i,]
-	
-		cat(as.character(data[["IDX_ID"]]), "")
-		cat(as.character(data[["TC_ID"]]), "")
-		cat(sprintf("%d", data[["SAMPLING_FACTOR"]]), "")
-		cat(sprintf("%d", data[["FCST_SIZE"]]), "")
-		cat(sprintf("%d", data[[paste("FCST_", op_name, "_U_TIME", sep="", collapse="")]]))
+	for(delta in sampling_rates) {
+		cat(sprintf("%d", delta))
+		for(idx_idx in 1:nrow(idx_types)) {
+			idx_name = paste(as.character(idx_types[idx_idx, "IDX_ID"]), delta, sep="_")
+			
+			data<-raw[raw$IDX_ID==idx_name,]
+			cat("", sprintf("%d", data[["FCST_SIZE"]]))
+			cat("", sprintf("%d", data[[paste("FCST_", op_name, "_U_TIME", sep="", collapse="")]]))
+		}
 		cat("\n")
 	}
 }
